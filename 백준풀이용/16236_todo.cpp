adc40580e75c0daca3188dfedd4a3c16fb60d6f9
ThinkMuk <b2000kang@gmail.com>
2022-04-03 22:52:21 +0900

    2022-04-03 16236 to be solved

--- 백준풀이용/16236_todo.cpp
+++ 백준풀이용/16236_todo.cpp
@@ -2,7 +2,10 @@
 #include <vector>
 #include <queue>
 #include <algorithm>
+#include <limits>
 using namespace std;
+
+//문제점: 횟수가 필요 이상으로 많이 나옴 (컴파일 다시 해서 문제점 파악 필수)
 
 int N;
 vector<vector<int>> fishBoard;
@@ -13,22 +16,23 @@ bool callShark = false;
 int sharkSize = 2;
 int sharkStack = 0;
 int totalTime = 0;
+vector<pair<int, pair<int, int>>> saveDistance;
 
 int dy[4] = { 0,1,0,-1 };
 int dx[4] = { -1,0,1,0 };
 
-void sharkStackCheck(int tempStack, int tempSize) //임이의 상어 스텍을 늘리면서, 상어의 성장크기 갱신
+void sharkStackCheck() //상어 스텍을 늘리면서, 상어의 성장크기 갱신
 {
-	tempStack++;
-	if (tempStack == tempSize) {
-		tempStack = 0;
-		tempSize++;
+	sharkStack++;
+	if (sharkStack == sharkSize) {
+		sharkStack = 0;
+		sharkSize++;
 	}
 }
 
 void targetMove() { //목표 물고기의 좌표를 저장하고 있는 벡터 위치를 옮기는 함수
 	cordMove.second++;
-	if (fishCord[cordMove.first][cordMove.second].first == NULL) {
+	if (cordMove.second >= fishCord[cordMove.first].size()) { //여기서 오류
 		cordMove.first++;
 		cordMove.second = 0;
 	}
@@ -51,8 +55,6 @@ void BFS(int y, int x)
 {
 	vector<vector<int>> tempShark;
 	queue<pair<int, int>> q;
-	int tmpSize = sharkSize;
-	int tmpStack = sharkStack;
 
 	for (int i = 0; i < N; i++)
 	{
@@ -92,7 +94,6 @@ void BFS(int y, int x)
 					}
 					else if (fishBoard[tmpY][tmpX] < sharkSize) { //만약 상어가 클 시
 						q.push(make_pair(tmpY, tmpX));
-						sharkStackCheck(tmpStack, tmpSize); //스텍 올리면서 체크
 						fishBoard[tmpY][tmpX] = 0; //그 자리는 이제 빈 칸
 						tempShark[tmpY][tmpX] = tempShark[cur.first][cur.second] + 1;
 					}
@@ -105,20 +106,13 @@ void BFS(int y, int x)
 		}
 	}
 
-	//** 여기가 문제임 **//
-	//목표물 좌표 받기
 	int tmpTime = tempShark[fishCord[cordMove.first][cordMove.second].first][fishCord[cordMove.first][cordMove.second].second];
 	if (tmpTime == -2) {//좌표가 -2인지 이동 횟수인지 확인
-		cout << totalTime << "\n";
-		callShark = true;
+		saveDistance.push_back(make_pair(INT_MAX, make_pair(fishCord[cordMove.first][cordMove.second].first, fishCord[cordMove.first][cordMove.second].second)));
 	}
-	else { //이동 횟수이면 총 이동횟수에 추가
-		totalTime += tmpTime;
-		sharkSize += tmpSize;
-		sharkStack += tmpStack;
-		sharkCord = make_pair(fishCord[cordMove.first][cordMove.second].first, fishCord[cordMove.first][cordMove.second].second);
+	else {
+		saveDistance.push_back(make_pair(tmpTime, make_pair(fishCord[cordMove.first][cordMove.second].first, fishCord[cordMove.first][cordMove.second].second)));
 	}
-
 }
 
 int main()
@@ -146,8 +140,65 @@ int main()
 		fishBoard.push_back(tempArr);
 	}
 
-	while (!callShark)
+
+	for (int i = 0; i < 9; i++)
 	{
-		BFS(sharkCord.first, sharkCord.second);
+		if (fishCord[i].empty())
+			continue;
+		else
+		{
+			for (int j = 0; j < fishCord[i].size(); j++)
+			{
+				BFS(sharkCord.first, sharkCord.second);
+				targetMove();
+			}
+			//여기서 걸린 시간을 모두 계산
+			sort(saveDistance.begin(), saveDistance.end());
+			int tmpSaveCord = 0;
+			for (int j = 0; j < saveDistance.size(); j++)
+			{
+				if (saveDistance[j].first != INT_MAX) {
+					sharkStackCheck();
+					totalTime += saveDistance[j].first;
+					sharkCord = make_pair(saveDistance[j].second.first, saveDistance[j].second.second);
+				}
+				else {//상어가 접근할 수 없는 곳이 생겼을 때 (INT_MAX가 나왔을 때)
+					if (saveDistance[0].first == INT_MAX) { //1마리밖에 없어 무조건 엄마상어를 불러야 할 때
+						callShark = true;
+						break;
+					}
+					else { //그래도 그동안 먹어온게 있어서 시도는 해볼 수 있을때
+						sharkCord = make_pair(saveDistance[j - 1].second.first, saveDistance[j - 1].second.second);
+						break;
+					}
+				}
+			}
+			//다시 벡터 초기화 후 반복
+			saveDistance.clear();
+			vector<pair<int, pair<int, int>>>().swap(saveDistance);
+		}
 	}
+
+	cout << totalTime << "\n";
 }
+
+
+////** 여기가 문제임 **//
+////목표물 좌표 받기
+//int tmpTime = tempShark[fishCord[cordMove.first][cordMove.second].first][fishCord[cordMove.first][cordMove.second].second];
+//if (tmpTime == -2) {//좌표가 -2인지 이동 횟수인지 확인
+//	cout << totalTime << "\n";
+//	callShark = true;
+//}
+//else { //이동 횟수이면 총 이동횟수에 추가
+//	totalTime += tmpTime;
+//	sharkSize += tmpSize;
+//	sharkStack += tmpStack;
+//	sharkCord = make_pair(fishCord[cordMove.first][cordMove.second].first, fishCord[cordMove.first][cordMove.second].second);
+//}
+
+//else if (fishBoard[tmpY][tmpX] < sharkSize) { //만약 상어가 클 시
+//	q.push(make_pair(tmpY, tmpX));
+//	fishBoard[tmpY][tmpX] = 0; //그 자리는 이제 빈 칸
+//	tempShark[tmpY][tmpX] = tempShark[cur.first][cur.second] + 1;
+//}
